da2687c0969183d8fa2cb80fa7a9171681543356
wenjie <1569925@qq.com>
2019-06-14 15:41:17 +0800

    策略模式

--- src/com/kok/designpatterns/Main.java
+++
@@ -1,9 +0,0 @@
-package com.kok.designpatterns;
-
-/**
- * @author wenjie
- * @description
- * @time 2019/6/13 0013 16:05
- */
-public class Main {
-}
---
+++ src/com/kok/designpatterns/strategy/SmsStrategy.java
@@ -0,0 +1,13 @@
+package com.kok.designpatterns.strategy;
+
+/**
+ * @author wenjie
+ * 短信发送接口
+ * @description
+ * @time 2019/6/14 0014 15:16
+ */
+public interface SmsStrategy {
+
+    void sendSms(SmsInfoDto smsInfoDto);
+
+}
