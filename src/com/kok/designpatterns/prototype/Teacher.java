df19488fe58cd23cc521d3499d7b96b3a676c5b2
wenjie <1569925@qq.com>
2019-06-14 16:45:22 +0800

    原型模式（Prototype Pattern）

---
+++ src/com/kok/designpatterns/prototype/Teacher.java
@@ -0,0 +1,28 @@
+package com.kok.designpatterns.prototype;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author wenjie
+ * @description
+ * @time 2019/6/14 0014 16:42
+ */
+@Getter
+@Setter
+public class Teacher implements Cloneable {
+
+    private String name;
+
+    private Integer age;
+
+    /**
+     * 浅拷贝
+     * @return
+     * @throws CloneNotSupportedException
+     */
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
---
+++ src/com/kok/designpatterns/prototype/Test.java
@@ -0,0 +1,20 @@
+package com.kok.designpatterns.prototype;
+
+/**
+ * @author wenjie
+ * @description
+ * @time 2019/6/14 0014 16:43
+ */
+public class Test {
+
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Teacher teacher = new Teacher();
+        teacher.setName("jax");
+        teacher.setAge(25);
+
+        Teacher teacherClone = (Teacher) teacher.clone();
+        Integer age = teacherClone.getAge();
+        System.out.println(age);
+    }
+
+}
